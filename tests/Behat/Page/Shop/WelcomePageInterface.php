9fa6bad944cb0e0557032fb31d7267c95dc7f913
Alexandre Balmes <alexandre@vanoix.com>
2021-03-18 17:29:54 +0100

    Update to Sylius 1.9

---
+++ tests/Behat/Page/Shop/WelcomePageInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Black\Tests\SyliusCookieAlertPlugin\Behat\Page\Shop;
+
+use FriendsOfBehat\PageObjectExtension\Page\SymfonyPageInterface;
+
+interface WelcomePageInterface extends SymfonyPageInterface
+{
+    /**
+     * @return string
+     */
+    public function getGreeting(): string;
+}
